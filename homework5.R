c59cd700939ae9ca9f54227a74fef345b1a1388c
Frederik Ledoux <frits@ledoux-grathem.nl>
2024-04-09 19:50:08 +0100

    Fixed issues with question 2.4

--- homework5.R
+++ homework5.R
@@ -23,7 +23,6 @@ clustered.summary <- function(reg, clusters) {
   return(reg_summary)
 }
 
-
 #	Question 2.1
 reg.1.lnavwage <- lm(ln_avwage ~ as.factor(lowwage) + as.factor(post) + as.factor(lowwage) * as.factor(post), data = data)
 reg.1.netpcm <- lm(net_pcm ~ as.factor(lowwage) + as.factor(post) + as.factor(lowwage) * as.factor(post), data = data)
@@ -39,16 +38,15 @@ reg.3.lnavwage.summary <- clustered.summary("reg.3.lnavwage", "regno")
 reg.3.netpcm <- lm(net_pcm ~ as.factor(regno) + as.factor(year) + as.factor(lowwage) * as.factor(post), data = data)
 reg.3.netpcm.summary <- clustered.summary("reg.3.netpcm", "regno")
 
-
 #	Question 2.4
-reg.4.lnavwage.industrytrend <- lm(ln_avwage ~ as.factor(regno) + as.factor(year) * as.factor(sic2) + as.factor(lowwage) * as.factor(post), data = data)
+reg.4.lnavwage.industrytrend <- lm(ln_avwage ~ as.factor(regno) + year * as.factor(sic2) + as.factor(lowwage) * as.factor(post), data = data)
 reg.4.lnavwage.industrytrend.summary <- clustered.summary("reg.4.lnavwage.industrytrend", "regno")
 
-reg.4.netpcm.industrytrend <- lm(net_pcm ~ as.factor(regno) + as.factor(year) * as.factor(sic2) + as.factor(lowwage) * as.factor(post), data = data)
+reg.4.netpcm.industrytrend <- lm(net_pcm ~ as.factor(regno) + year * as.factor(sic2) + as.factor(lowwage) * as.factor(post), data = data)
 reg.4.netpcm.industrytrend.summary <- clustered.summary("reg.4.netpcm.industrytrend", "regno")
 
-reg.4.lnavwage.firmtrend <- lm(ln_avwage ~ as.factor(regno) + as.factor(year) * as.factor(regno) + as.factor(lowwage) * as.factor(post), data = data)
+reg.4.lnavwage.firmtrend <- lm(ln_avwage ~ as.factor(regno) + year * as.factor(regno) + as.factor(lowwage) * as.factor(post), data = data)
 reg.4.lnavwage.firmtrend.summary <- clustered.summary("reg.4.lnavwage.firmtrend", "regno")
 
-reg.4.netpcm.firmtrend <- lm(net_pcm ~ as.factor(regno) + as.factor(year) * as.factor(regno) + as.factor(lowwage) * as.factor(post), data = data)
+reg.4.netpcm.firmtrend <- lm(net_pcm ~ as.factor(regno) + year * as.factor(regno) + as.factor(lowwage) * as.factor(post), data = data)
 reg.4.netpcm.firmtrend.summary <- clustered.summary("reg.4.netpcm.firmtrend", "regno")
